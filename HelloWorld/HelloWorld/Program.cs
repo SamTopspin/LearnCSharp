6e72ea154f259b5bbbd85a2eb4c29f44f50ebe7d
Sameer Sankhyadhar <Sameer.Sankhyadhar@waikatoregion.govt.nz>
2020-03-28 19:14:27 +1300

    Initial Commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // my code goes here
+
+            Console.Write("\n\tWhat is your name buddy?\n\n\t");
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"\n\nHello {name}");
+            Console.WriteLine($"\n\nCurrent time is {DateTime.Now}");
+
+            int dayOfYear = DateTime.Now.DayOfYear;
+            Console.Write("Day of year: ");
+            Console.WriteLine(dayOfYear);
+
+
+
+        }
+    }
+}
